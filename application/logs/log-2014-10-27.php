75fe5918110fe1da1ae9532c20ed0bb67d8e53d9
aswanesher <aswansetiawan01@gmail.com>
2014-10-28 03:35:04 +0700

    Log

---
+++ application/logs/log-2014-10-27.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-10-27 21:30:42 --> Severity: Notice  --> Undefined variable: comment_allow D:\wamp\www\ululazmi\system\core\Loader.php(679) : eval()'d code 13
+ERROR - 2014-10-27 21:30:43 --> Severity: Notice  --> Undefined variable: comment_allow D:\wamp\www\ululazmi\system\core\Loader.php(679) : eval()'d code 13
+ERROR - 2014-10-27 21:30:56 --> Severity: Notice  --> Undefined variable: comment_allow D:\wamp\www\ululazmi\system\core\Loader.php(679) : eval()'d code 13
+ERROR - 2014-10-27 21:30:59 --> Severity: Notice  --> Undefined variable: comment_allow D:\wamp\www\ululazmi\system\core\Loader.php(679) : eval()'d code 13
