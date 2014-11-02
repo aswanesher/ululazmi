eb6112a9a7743ad1ab993b9431396bf70b19686f
aswanesher <aswansetiawan01@gmail.com>
2014-11-02 11:52:24 +0700

    ok

---
+++ application/logs/log-2014-11-02.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-11-02 04:25:06 --> Could not find the language key : "categories"
+ERROR - 2014-11-02 04:25:06 --> Could not find the language key : "categories"
+ERROR - 2014-11-02 04:25:06 --> Could not find the language key : "categories"
+ERROR - 2014-11-02 04:25:13 --> Could not find the language key : "categories"
+ERROR - 2014-11-02 04:25:19 --> Could not find the language key : "categories"
+ERROR - 2014-11-02 04:25:19 --> Could not find the language key : "categories"
+ERROR - 2014-11-02 04:25:19 --> Could not find the language key : "categories"
